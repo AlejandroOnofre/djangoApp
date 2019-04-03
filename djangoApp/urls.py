d5942a7912915c8f8f8d29903474d79b118301dc
AlejandroOnofre <alejandro.onofre@bloque9.com>
2019-04-03 12:48:29 -0600

    ADD users endpoint

--- djangoApp/urls.py
+++ djangoApp/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from general.views import UserViewSet
 from rest_framework import routers
 from django.conf.urls import url, include
 from rest_framework_simplejwt.views import (
@@ -22,10 +23,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     url(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^users/', include(router.urls)),
 ]
